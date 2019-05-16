33c3605d2f64c23eb8e41bad7e7058310a357b1e
Rushil <pererarushil1940@gmail.com>
2019-05-16 04:43:40 -0400

    Tweaked designs for login and voting pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import { GoogleLogin } from 'react-google-login/index';
+import { GoogleLogin } from 'react-google-login';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import request from '../network';
 
@@ -33,23 +34,31 @@ function Login() {
 
   if (!isSignedIn) {
     return (
-      <GoogleLogin
-        autoLoad
-        clientId="1081480094053-t8ii15jeout0pedl3riq55suvnu4hhub.apps.googleusercontent.com"
-        buttonText="Login"
-        hostedDomain="uottawa.ca"
-        onSuccess={loginResponse}
-        onFailure={loginResponse}
-      />
+      <>
+        <Typography variant="h6">
+          <strong>
+            We take voter anonymity seriously. We make you log in to your uOttawa account to track that you have voted but we can never trace back to
+            who you voted for.
+          </strong>
+        </Typography>
+        <GoogleLogin
+          clientId="911039919657-o235c8rl1qr59hlnr2djoiuivqbqmib2.apps.googleusercontent.com"
+          buttonText="Login"
+          hostedDomain="uottawa.ca"
+          onSuccess={loginResponse}
+          onFailure={loginResponse}
+        />
+      </>
     );
   }
 
   if (!isValidEmail) {
     return (
-      <div>
-        <h3>Sorry, {name}! {email} is an invalid email, please sign in with your uOttawa email.</h3>
-        <button>Okay</button>
-      </div>
+      <>
+        {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+        <Typography variant="h5">Sorry, {name}! {email} is an invalid email, please sign in with your uOttawa email.</Typography>
+        <Button variant="contained">Okay</Button>
+      </>
     );
   }
 
--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -3,16 +3,17 @@ import Card from '@material-ui/core/Card/index';
 import CardHeader from '@material-ui/core/CardHeader/index';
 import CardContent from '@material-ui/core/CardContent/index';
 import CircularProgress from '@material-ui/core/CircularProgress/index';
-import Typography from '@material-ui/core/Typography/index';
 import FormControl from '@material-ui/core/FormControl/index';
 import FormControlLabel from '@material-ui/core/FormControlLabel/index';
 import FormLabel from '@material-ui/core/FormLabel/index';
 import Radio from '@material-ui/core/Radio/index';
 import RadioGroup from '@material-ui/core/RadioGroup/index';
 import CardActions from '@material-ui/core/CardActions/index';
+import Snackbar from '@material-ui/core/Snackbar';
 import Button from '@material-ui/core/Button/index';
 import { makeStyles } from '@material-ui/styles';
 
+import MessageCard from '../components/MessageCard';
 import request from '../network';
 
 const useStyles = makeStyles({
@@ -32,6 +33,40 @@ function Vote() {
   const [isLoading, setLoading] = useState(true);
   const [candidatesObj, setCandidates] = useState([]);
   const [selected, setSelected] = useState([]);
+
+  let counter = 0;
+  const messageQueue = [];
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [messageInfo, setMessageInfo] = useState({});
+
+  function showMessage(message) {
+    messageQueue.push({
+      // eslint-disable-next-line no-plusplus
+      key: counter++,
+      message,
+    });
+
+    if (showSnackbar) {
+      setShowSnackbar(false);
+    } else {
+      processQueue();
+    }
+  }
+
+  function processQueue() {
+    if (messageQueue.length > 0) {
+      setMessageInfo(messageQueue.shift());
+      setShowSnackbar(true);
+    }
+  }
+
+  function handleSnackbarClose() {
+    setShowSnackbar(false);
+  }
+
+  function handleSnackbarExit() {
+    processQueue();
+  }
 
   async function getCandidates() {
     const { body: data } = await request.get('/vote/candidates');
@@ -58,7 +93,7 @@ function Vote() {
     return (
       <div>
         <CircularProgress />
-        <Typography variant="body2">Loading...</Typography>
+        <MessageCard message="Loading..." />
       </div>
     );
   }
@@ -86,36 +121,53 @@ function Vote() {
   }
 
   return (
-    <Card className={classes.root}>
-      <CardHeader className={classes.title} title="Vote for your next ..." />
-      <CardContent>
-        <FormControl component="fieldset">
-          {
-            Object.entries(candidatesObj)
-              .map(([position, candidates], index) => (
-                <div key={position}>
-                  <FormLabel key={`${position}-header`} component="legend" className={index > 0 ? classes.subHeader : ''}>{position}</FormLabel>
-                  <RadioGroup aria-label={position} name={position} value={selected[position].toString()} onChange={handleChange}>
-                    {candidates.map(({ id, name }) => (
-                      <FormControlLabel
-                        key={`${position}-${name}`}
-                        control={<Radio color="secondary" />}
-                        label={name}
-                        value={id.toString()}
-                      />
-                    ))}
-                  </RadioGroup>
-                </div>
-              ))
-          }
-        </FormControl>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="secondary" onClick={() => submitVote()}>
-          Submit
-        </Button>
-      </CardActions>
-    </Card>
+    <>
+      <Card className={classes.root}>
+        <CardHeader className={classes.title} title="Vote for your next ..." />
+        <CardContent>
+          <FormControl component="fieldset">
+            {
+              Object.entries(candidatesObj)
+                .map(([position, candidates], index) => (
+                  <div key={position}>
+                    <FormLabel key={`${position}-header`} component="legend" className={index > 0 ? classes.subHeader : ''}>{position}</FormLabel>
+                    <RadioGroup aria-label={position} name={position} value={selected[position].toString()} onChange={handleChange}>
+                      {candidates.map(({ id, name }) => (
+                        <FormControlLabel
+                          key={`${position}-${name}`}
+                          control={<Radio color="secondary" />}
+                          label={name}
+                          value={id.toString()}
+                        />
+                      ))}
+                    </RadioGroup>
+                  </div>
+                ))
+            }
+          </FormControl>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="secondary" onClick={() => submitVote()}>
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+      <Snackbar
+        key={messageInfo.key}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        open={showSnackbar}
+        autoHideDuration={2000}
+        onClose={handleSnackbarClose}
+        onExited={handleSnackbarExit}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">{messageInfo.message}</span>}
+      />
+    </>
   );
 }
 
