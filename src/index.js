d39bc2ed26eb11eb5c4e6c6ab50881efca3471d5
Rushil <pererarushil1940@gmail.com>
2019-05-17 02:04:09 -0400

    Added error reporting with Sentry

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
+import * as Sentry from '@sentry/browser';
 
 import App from './pages/App';
 
 import './index.css';
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+  });
+}
 
 const theme = createMuiTheme({
   palette: {
